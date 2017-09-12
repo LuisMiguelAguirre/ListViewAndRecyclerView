a6e9a45358caf332ab5e8825030f31542c13e4ff
Luis Aguirre <kourosvi@gmail.com>
2017-09-12 16:40:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/listviewandrecyclerview/ListViewActivity.java
@@ -0,0 +1,33 @@
+package com.example.admin.listviewandrecyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    ListView lvSimple, lvCustom, lvCustomVH;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvCustom = (ListView) findViewById(R.id.lvCustom);
+        lvCustomVH = (ListView) findViewById(R.id.lvCustomVH);
+
+        List<String> personList = new ArrayList<>();
+        personList.add("Jhon Doe");
+        personList.add("Jhon Doe1");
+        personList.add("Jhon Doe2");
+        personList.add("Jhon Doe3");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, personList);
+        lvSimple.setAdapter(arrayAdapter);
+    }
+}
